e43b0ff85f5e5deaa2d53a230fa4791545577b93
franco paez <francopaez_11@hotmail.com>
2024-05-24 01:37:50 -0300

    implementacion de dom y eventos

--- script/script.js
+++ script/script.js
@@ -1,117 +1,100 @@
-//el programa es simulador de compras de libros,pedira al usuario datos de autor nombre y entrega y generara su pedido
+///variables del programa
+//arreglo de pedidos vacio
+let arregloPedidos=[]
+//variable de los divs
+let divConfirmaciones=document.querySelector(".confirmaciones")
+//variables de los botones
+let botonCargar=document.querySelector("#cargar")
+let botonRefrescar=document.querySelector("#refrescar")
+///variables de los inputs
+let inputNombre=document.querySelector("#nombre")
+let inputAutor=document.querySelector("#autor")
+let inputDireccion=document.querySelector("#direccion")
+//variable de la ul
+let listaDesordenada=document.querySelector("#listaDesordenada")
+//variable del espan para generar numero de pedido aleatorio
+let numeroPedido=document.querySelector("#pedido")
 
-//alerts
-alert("En este programa deberas realizar un pedido de libros y a donde deseas resivirlo")
-alert("ATENCION!!SOLO PUEDES SOLICITAR HASTA 10 LIBROS")
-alert("Por favor selecciona una opcion: \n 1)Ver listado de libros no disponibles \n 2)Solicitar libro \n 3)Realizar pedido  \n 4)Ver listado de libros recien agregados al stock  \n 5)recomendacion de libro " )
-
-//variable que inicia el ciclo switch
-let opcionUsuario=prompt("ingrese una opcion")
-switch(opcionUsuario){
-   case "1":
-    //arreglo utilizado para mostrar los libros no disponibles  
-   librosNoDisponibles=[" Harry Potter y el legado maldito.","\n Harry Potter y la piedra filosofa "," \n Maze Runner-Virus letal - El comienzo (renovación)","\n Inteligencia emocional (Daniel Goleman)","\n El miedo a la Libertad (Erich Fromm)","\n Air. La historia de Michael Jordan"]
-   alert(librosNoDisponibles)
-
-   break
-   case "2":
-      //arreglo vacio que se ire completando atravez de un while con los datos que brinde el usuario
-      let librosAgregados=[]
-      alert("En esta seccion podra solicitar nombres de libros que le gustaria que sumemos a nuestro listado")
-      alert("!!IMPORTANTE!!,PARA SALIR ESCRIBA ESC")
-      let usuario=prompt("ingrese nombre de libros que le gustaria que sumemos a nuestro listado")
-      while(usuario.toLowerCase()!="esc"){
-         librosAgregados.push(usuario)
-         usuario=prompt("ingrese nombre del libro")
-         if(usuario.toLowerCase()=="esc"){
-            alert("Los libros que solicitaste fueron : " +"\n"+librosAgregados )
-            alert("Gracias por solicitar,pronto los estaremos sumando a nuestro listado")
-         }
-      }
-
-
-   break
-   case "3":
-      //variables del case 3
-      let numeroDePedidos=parseInt(prompt("Cuantos libros deseas solicitar?"))
-      let contador=0
-      let ingresados=""
-
-   //bucle que pedira datos al usuario hasta que la condicion se cumpla
-      while(numeroDePedidos<=10 && contador<numeroDePedidos){
-         let nombreLibro=prompt("ingrese nombre del libro")
-         let autor=prompt("ingrese autor")
-         let direccion=prompt("ingrese direccion")
-
-         //variables ingresados con strings vacia que se ira completando a medida que el contador incremente su vuelta con datos del usuario
-         ingresados +="el libro "+ nombreLibro +" del autor " +autor + " sera enviado a "+ direccion +"\n"
-         contador=contador+1
-
-         alert(ingresados)
-
-          
-      }
-
-   break
-   case "4":
-      alert("aqui podes ver el listado de libros que aregamos recientemente a nuestro stock")
-      //instanciamos un objeto nuevo que iremos completando e implementando dentro del arreglo vacio de la variable libros nuevos
-      class Libro{
-         constructor(nombre,nuevo,autor){
-            this.nombre=nombre
-            this.nuevo=nuevo
-            this.autor=autor
-         }
-      }
-      
-      let librosNuevos=[]
-      //agregamos los libros nuevos al arreglo vacio con el metodo push
-      librosNuevos.push(new Libro("Los vecinos mueren en las novelas",true,"Sergio Aguirre"))
-      librosNuevos.push(new Libro("Mi planta de naranja lima",false,"José Mauro de Vasconcelos"))
-      librosNuevos.push(new Libro("Campos de fresas",false,"Jordi Sierra i Fabra"))
-      librosNuevos.push(new Libro("El principito",true,"Antoine de Saint-Exupéry"))
-      librosNuevos.push(new Libro("En agosto nos vemos",true,"Gabriel García Márquez"))
-      
-
-      //con for each recorremos el arreglo mostrando los libros agregados
-       librosNuevos.forEach(function(libro){
-              alert(libro.nombre)
-          })
-
-          alert("Acontinuacion te detallamos los libros que se encuentran usados pero en buen estado")
-
-          //creamos una funcion de corroborar que usara el metodo filter para mostrar los libros con estado usado(false)
-          function corroborarEstadoDeLibro(){
-          let buscarEstadoDeLibro=librosNuevos.filter(condicionDeLibro=>condicionDeLibro.nuevo===false)
-          //dentro iniciamos un for each para que nos muestre el nombre de los libros usados
-         buscarEstadoDeLibro.forEach(function(estado){
-            alert(estado.nombre)
-         })
-      }corroborarEstadoDeLibro()
-
-   break
-   case"5":
-   //aca el programa recomendara al usuario un libro al azar
-   //0=LA AVENTURA MAS GRANDE JAMAS CONTADA (LOS 4 FANTASTICOS)-| ERIK LARSEN |
-   //1=A LA CAZA DEL HACKER-| BRUCE STERLING |
-   //2=JUVENTUD,REBELDIA Y REVOLUCION-| ALEJANDRO SAN FRANCISCO |
-   alert("Aca el programa te recomendara un libro al azar,esperamos lo disfrutes")
-   //creamos la funcion libroalazar que tendra una variable con match random para elegir un libro aleatrio entre los tres mencionados arriba
-   function libroAlAzar(){
-      let computadora=Math.floor(Math.random()*3)
-
-      if(computadora==0){
-         alert("Podria recomendarte : LA AVENTURA MAS GRANDE JAMAS CONTADA (LOS 4 FANTASTICOS)-| ERIK LARSEN |")
-      }else if(computadora==1){
-         alert("Podria recomendarte : A LA CAZA DEL HACKER-| BRUCE STERLING |")
-      }else if(computadora==2){
-         alert("Podria recomendarte : JUVENTUD,REBELDIA Y REVOLUCION-| ALEJANDRO SAN FRANCISCO |")
-      }
-      
-   }libroAlAzar()
-  break
-          
-   default:
-      alert("!!ERROR!!,no selecionaste una opcion valida")
+//funciones
+//funcion para borrar todos los inputs
+function borrarInputs(){
+   inputNombre.value=""
+   inputAutor.value=""
+   inputDireccion.value=""
 }
-                    
+
+//funcion para generar un numero al azar para simular un numero de pedidos
+function randomNumber(){
+   let numeroRandom=Math.floor(Math.random()*1000)
+   return numeroRandom
+}
+
+//funcion para borrar numero de pedidos
+function borrarNroPedido(){
+   numeroPedido.textContent=""
+}
+
+
+//instanciamos un objeto con calse pedido que tiene su metodo crearitems
+class Pedido{
+   constructor(nombre,autor,direccion){
+      this.nombre=nombre
+      this.autor=autor
+      this.direccion=direccion
+      this.cargarItems(nombre,autor,direccion)
+   }
+   ///el metodo creara los li que iran dentro de los ul
+   cargarItems(nombre,autor,direccion){
+      let listItems=document.createElement("li")
+      listItems.textContent="✓ El libro titulado " + nombre + ", del autor  " + autor + " será llevado a  la dirección " + direccion
+      listaDesordenada.appendChild(listItems)
+   }
+
+}
+
+//definimos el comportamiento de los botones
+//boton cargar
+//en la funcion definira el numero de pedidos maximo es 5 en ese caso el condicional valida
+//que se llenen los campos y que al cargarlos no supere 5 pedidos
+botonCargar.addEventListener("click",function(){
+   if (inputNombre.value && inputAutor.value && inputDireccion.value){
+      //en este condicional vamos validando con el obejto instanciado y tomando el valor de cada input con value
+      //y se ira agregando al arreglo vacio con push
+if(arregloPedidos.length < 5){
+   let item=new Pedido(inputNombre.value,inputAutor.value,inputDireccion.value )
+   borrarInputs()
+   arregloPedidos.push(item)
+}else{alert("Has llegado al limite de libros por pedido")
+   borrarInputs()
+}}
+else {alert("no han sido completado todos los campos")
+   borrarInputs()
+   //en este condicional generamos un nuero de pedido al azar
+}if (arregloPedidos.length == 1){
+        
+   numeroPedido.textContent = "Nro. de pedido: " + randomNumber()
+}
+
+})
+
+
+//boton refrescar
+botonRefrescar.addEventListener("click",function(){
+   //seleccionamos todos los li y los recorremos al arreglo con for of y los removemos de la lista
+   let itemsLista=document.querySelectorAll("li")
+   for(let value of itemsLista){
+      listaDesordenada.removeChild(value)
+   }
+   arregloPedidos=[]
+   borrarInputs()
+   borrarNroPedido()
+})   
+
+
+
+
+
+
+
+
+
